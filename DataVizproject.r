5e8853170d8a118d4cc95992e8b0ffa649c4bc92
Tobin Haefele <143435198+tobin-haefele@users.noreply.github.com>
2023-09-23 11:12:04 -0600

    Update DataVizproject.r

--- DataVizproject.r
+++ DataVizproject.r
@@ -2,13 +2,26 @@
 library(tidyverse)
 #Load dataset in
 CarData = read.csv(file = "dv-carbitrage-raw-data.csv", header = TRUE)
-names(CarData)
-CarData_filtered = CarData %>% 
+#filter so you only have make/model columns
+CarData_filtered = CarData %>%
     select(make, model)
-names(CarData_filtered)
-for(i in length(CarData_filtered$make)
-    if(CarData_filtered$make[i] = CarData_filtered$make[(i-1)]
-        #create list sorted by make of [i]
-    )
-    
-    )
+make_model_counts = make_model_counts %>%
+  filter(!is.na(make) & !is.na(model))
+#count the repeats of each make and model
+make_model_counts = CarData_filtered %>%
+    group_by(make,model) %>%
+    summarise(count = n())
+#sort by descending order
+make_model_counts = make_model_counts %>%
+    arrange(desc(count))
+#filter by most popular
+most_popular_models = make_model_counts %>%
+  group_by(make) %>%
+  slice(1)
+#create a bar plot
+ggplot(data = most_popular_models, aes(x = make, y = model, size = count)) +
+  geom_point() +
+  scale_size_area(max_size = 10) +
+  xlab("Make") +
+  ylab("Model") +
+  ggtitle("Most Popular Model for Each Car Make")
