1ee37a5e7685bc6e2baa86ad11f7f2104e5636cc
Tobin Haefele <143435198+tobin-haefele@users.noreply.github.com>
2023-11-05 10:52:01 -0700

    uoptade

--- Dashboard/dashboardFile.R
+++ Dashboard/dashboardFile.R
@@ -7,13 +7,13 @@ cdata = read.csv("Dashboard\\listing-data.csv")
 
 # Load data and clean out empty make and price values
 cdata <- read.csv("Dashboard/listing-data.csv")
-cdata <- cdata %>% filter(make != "" & price != "" & time_posted != "" & model != "")
+cdata <- cdata %>% filter(make != "" & price != "" & time_posted != "" & model != "", condition != "")
 
 # Define UI
 ui <- dashboardPage(
   dashboardHeader(title = "Kelly Blue Book"),
   dashboardSidebar(
-    sidebarMenu(\
+    sidebarMenu(
       menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
       menuItem("Widgets", tabName = "widgets", icon = icon("th"))
     )
@@ -23,22 +23,33 @@ ui <- dashboardPage(
       # First tab content
       tabItem(tabName = "dashboard",
               fluidRow(
+              
                 box(
-                  title = "Median Price Over Time",
-                  plotOutput("plot1", height = 250)
-                ),
-                box(
-                  title = "Top 10 Most Popular Makes",
+                  title = "Top 10 Most Popular Models",
                   plotOutput("plot2", height = 400)
-                ),
-                box(
-                  title = "Top 10 Most Popular Locations",
-                  tableOutput("table1")
                 ),
                 box(
                   title = "Age of Cars Posted",
                   plotOutput("plot3", height = 400)
                 )
+              ),
+              fluidRow(
+                width = 6,
+                box(
+                  title = "Median Price Over Time",
+                  plotOutput("plot1", height = 400)
+                ),
+                box(
+                  title = "Top 10 Most Popular Locations",
+                  tableOutput("table1"),
+                  width = 6
+                ),
+                box(
+                  title = "Key Findings",
+                  textOutput("text1")
+                )
+              ),
+              )
                 
 
               )
@@ -59,24 +70,24 @@ server <- function(input, output, session) {
   output$plot1 <- renderPlot({
 
     #Selecting data for price graph
-    cprice_filtered <- cdata %>% select(price, time_posted,make)
+    cprice_filtered <- cdata %>% select(price, time_posted,make, year)
     cprice_filtered$time_posted <- as.Date(cprice_filtered$time_posted)
     #filtering out the outliers price over 1 million or equal to 0
     cprice_filtered <- cprice_filtered %>% filter(price < 1000000 & price > 0)
+    head(cprice_filtered)
     #group by date and price and get the median price for each day
     cprice_filtered <- cprice_filtered %>% group_by(time_posted) %>% summarise(median_price = median(price))
+    head(cprice_filtered)
     #get max date for filtering
     max_date <- max(cprice_filtered$time_posted)
     #filter out data for the past quarter
     cprice_filtered <- cprice_filtered %>% filter(time_posted >= max_date - 91)
-    #plotting the graph
-    plot(cprice_filtered$time_posted, cprice_filtered$median_price, type = "l", xlab = "Date", ylab = "Median Price", main = "Median Price Over Last Quarter") +
-      theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-      geom_smooth(method = "lm", se = FALSE) +
-      geom_point() +
-      scale_x_date(date_breaks = "1 week", date_labels = "%m/%d/%Y") +
-      scale_y_continuous(labels = scales::dollar) +
-      theme(plot.title = element_text(hjust = 0.5))
+    #filter out data for models older than 1980
+    cprice_filtered <- cprice_filtered %>% filter(time_posted >= 1980)
+    #plot the graph
+    ggplot(cprice_filtered, aes(x = time_posted, y = median_price)) + geom_line() + labs(x = "Date", y = "Median Price", title = "Median Price Over Time") + theme(plot.title = element_text(hjust = 0.5))
+
+  
   })
   output$plot2 <- renderPlot({
     # Top 10 most popular models of cars posted on Craigslist in the past quarter (based on number of posts)
@@ -94,10 +105,6 @@ server <- function(input, output, session) {
       theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
       theme(plot.title = element_text(hjust = 0.5))
   })
-  output$text1 <- renderText({
-    # Key insights
-    "This dashboard provides an a quarterly overview of the used car market based on listings collected from Craigslist. Included are the median price of all cars over time, the top 10 most popular makes and models, and top 10 most popular locations based on number of posts in the past quarter."
-  }) 
   # Render the plot based on user input
   output$plot3 <- renderPlot({
     # histogram of number of posts by year of car listed
@@ -120,6 +127,14 @@ server <- function(input, output, session) {
       mutate(percentage = 100 * count / sum(count)) %>% 
       arrange(desc(percentage)) %>% 
       head(10)
+  })
+  output$text1 <- renderText({
+    #output key findings in bullet points
+    "Key Findings:
+    - The median price of cars posted on Craigslist has been increasing over the past quarter
+    - The most popular models of cars posted on Craigslist are Ford F150s, Honda Civics and Toyota Camrys
+    - The most popular locations for cars posted on Craigslist are San Francisco, Los Angeles, and Phoenix"
+
   })
 }
 # Run the Shiny app
